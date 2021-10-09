d9605bda9a00a3a7e54b5adfbf09650a9ca48f1f
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-09 23:51:40 +0300

    feat: add calculating polyharmonic signals

--- utils.py
+++ utils.py
@@ -3,6 +3,35 @@ import struct
 
 from stopwatch import Stopwatch
 
+def sin_taylor(x):
+	eps = 0.0000001
+	
+	n = 1
+	curr_value = x
+	sum_ = 0
+	
+	while abs(curr_value) > eps:
+		sum_ += curr_value
+		curr_value *= -(x * x) / ((n + 1) * (n + 2))
+		n += 2
+	
+	return sum_
+
+def calc_harmonic_signal(A, N, n, phi, f):
+	arg = (((2 * np.pi * f * n) / N) + phi)
+	if arg >= 2 * np.pi:
+		arg -= 2 * np.pi * (arg // (2 * np.pi))
+	return A * sin_taylor(arg)
+
+def calc_polyharmonic_signal(A_s, chunk_size, phi_s, f_s):
+	N = chunk_size
+	
+	data_x = np.arange(0, N)
+
+	data_y = np.zeros((N,), dtype=int)
+	for i, A in enumerate(A_s):
+		data_y = np.add(data_y, [calc_harmonic_signal(A, N, point_x, phi_s[i], f_s[i]) for point_x in data_x])
+
 def get_stopwatch(callback):
 	stopwatch = Stopwatch()
 	stopwatch.progress_signal.connect(callback)
